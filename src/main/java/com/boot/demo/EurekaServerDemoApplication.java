d9c9a20c370e821c8cdbffa430d720aea8edc7b9
ashish.sawant-ext@rexel.com <c2Wr4w2H>
2020-08-02 20:21:48 +0530

    Creating Eureka Server

--- src/main/java/com/boot/demo/EurekaServerDemoApplication.java
+++ src/main/java/com/boot/demo/EurekaServerDemoApplication.java
@@ -2,7 +2,9 @@ package com.boot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerDemoApplication {
 
